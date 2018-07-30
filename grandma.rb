b1755aad206092838fdf51c1b63287673fab0f40
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-07-30 18:07:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,9 @@ def speak_to_grandma (statement)
 
   if statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif statement == statement.uppercase
+  elsif statement == statement.upcase
     return "NO, NOT SINCE 1938!"
   else return "HUH?! SPEAK UP, SONNY!"
   end
-  
+
 end
